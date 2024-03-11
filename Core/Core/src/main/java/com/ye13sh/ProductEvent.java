b9ca0270dae3b374c589ebfbb4d06fe91d516ba3
Yeshwanthh Kumar <115858058+ye13sh@users.noreply.github.com>
2024-03-11 21:02:00 +0530

    Add files via upload

---
+++ Core/Core/src/main/java/com/ye13sh/ProductEvent.java
@@ -0,0 +1,50 @@
+package com.ye13sh;
+
+public class ProductEvent {
+    private String productID;
+    private String productName;
+    private int quantity;
+    private double price;
+
+    public ProductEvent() {
+    }
+
+    public ProductEvent(String productID, String productName, int quantity, double price) {
+        this.productID = productID;
+        this.productName = productName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
